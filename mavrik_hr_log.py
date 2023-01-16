82f4f911669d30097afa30ba70429dbe68d81009
Hussein Ahmed <59267542+Husseinbnahmed@users.noreply.github.com>
2023-01-15 22:39:32 -0500

    update code to download correctly

--- mavrik_hr_log.py
+++ mavrik_hr_log.py
@@ -4,20 +4,31 @@ import numpy as np
 import pickle
 from st_aggrid import AgGrid
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 st.markdown(""" ### How It Works 	:grey_question:""")
 st.caption("""Unleash the power of automation with our cutting-edge employee hours worked tracking app.Effortlessly log and aggregate employee data, including 
 weekly hours and overtime worked, in a streamlined and dynamic manner. Say goodbye to tedious manual data entry and hello to increased efficiency and organization. :grinning:""")
 
 st.caption("""
-1. Start a new database by entering a database name
-2. Enter the employee's first and last name.
-3. Input the hours worked by the employee during the first week.
-4. Input the hours worked by the employee during the second week.
-5. Click the "add to database" button
-6. Observe the database automatically calculate the total overtime and regular hours worked.
+1. Type in the first and last name of the employee
+2. Type in the number of hours worked by the employee during the first week
+3. Type in the number of hours worked by the employee during the second week
+4. Click the "add to my database" button
+5. Watch the database automatically tally up the overtime and regular hours worked
 
 """)
-db_name = st.text_input(":file_folder: Create a new file name (example: week 20 ) ")
+db_name = st.text_input(":file_folder: Create a new database name (example: 1-15-2023 to 1-20-2023) ")
 class EmployeeData:
     def __init__(self):
         try:
@@ -46,28 +57,27 @@ class EmployeeData:
         with open(db_name+'.pkl', 'wb') as f:
             pickle.dump(self.df, f)
         return self.df
-    
+            
 employee_data = EmployeeData()
 
 # Function to switch name format
-def switch_name_format(name):
-    last_name, first_name = name.split(', ')
-    return f"{first_name} {last_name}"
+# def switch_name_format(name):
+#     last_name, first_name = name.split(', ')
+#     return f"{first_name} {last_name}"
 
 def create_aggrid(f):
     Agrid_output = AgGrid(f, editable=True, theme="streamlit", height=300)
     return Agrid_output
 
 
-
 ###### start of application --------------------------------------------------------------------------
 with st.sidebar:
     st.sidebar.image("Maverick Concierge Blue Logo-01.jpg")
-    st.title("TimeMaverick")
+    st.title("Mavrick's time tracking app")
     
-
     list = pd.read_csv(r"Book1.csv")['Full name']
-    employee_names = list.apply(switch_name_format)
+    # employee_names = list.apply(switch_name_format)
+    employee_names = list
     options = st.multiselect(":female-office-worker: Employee Name ", employee_names)
     firstWeek = st.number_input(":calendar: Week one (1) hours worked", step=0.01, min_value=0.0, help="50")
     secondWeek = st.number_input(":calendar: Week two (2) hours worked", step=0.01, min_value=0.0, help="60")
@@ -80,9 +90,9 @@ with st.sidebar:
 # st.dataframe(employee_data.df, width=1000)
 edited_df = create_aggrid(employee_data.df)
 
-
 @st.cache
 def convert_df(df):
+    df = df.sort_values(by="Name", ascending=True)
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
